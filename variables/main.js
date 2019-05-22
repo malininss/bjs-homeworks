a11742778239b400c0a7da14fdfcae81b94d9d48
Sergey Malinin <malininss@yandex.ru>
2019-05-22 21:40:58 +0300

    homework fixed

--- variables/main.js
+++ variables/main.js
@@ -1,6 +1,22 @@
+//1 exercise
 let algebra = 5,
     geography = 4,
     physics = 4;
 
 let average = Math.round((algebra + geography + physics) / 3); 
 console.log(average);
+
+//2 exercise
+let name = 'Сережа',
+    message = `Привет мир! Меня зовут ${name}`;
+
+console.log(message);
+
+//3 exercise
+let x = 2,
+    y = 22,
+    z = 00;
+
+let formula = x * y + 5 * z + x - 1;
+
+console.log(formula);
