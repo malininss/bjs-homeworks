6ac9d48d70212b8c81ee0206b7f27f90fbf98cd6
Sergey Malinin <malininss@yandex.ru>
2019-06-10 22:03:16 +0300

    exercise 2 done

--- functions/main.js
+++ functions/main.js
@@ -1,34 +1,78 @@
 'use strick'
 
-function getSolutions(a, b, c) {
-    let d = Math.pow(b, 2) - 4 * a * c;
-    if (d < 0) {
-        return { D: d };
-    } else if (d === 0) {
-        let x1 = -b / (2 * a);
+//Задание 1
 
-        console.log(x1);
-        return { roots: [ x1 ], D: d };
-    } else {
-        let x1 = (-b + Math.sqrt(d)) / (2 * a);
-        let x2 = (-b - Math.sqrt(d)) / (2 * a);
-        return { roots: [ x1, x2 ], D: d}
-    };
-};
+// function getSolutions(a, b, c) {
+//     let d = Math.pow(b, 2) - 4 * a * c;
+//     if (d < 0) {
+//         return { D: d };
+//     } else if (d === 0) {
+//         let x1 = -b / (2 * a);
 
-function showSolutionsMessage(a, b, c) {
-    let result = getSolutions(a, b, c);
-    console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
-    console.log(`Значение дискриминанта: ${result.D}`);
-    if (result.D < 0) {
-        console.log('Уравнение не имеет вещественных корней');
-    } else if (result.D === 0) {
-        console.log(`Уравнение имеет один корень X₁ = ${result.roots[0]}`);
-    } else {
-        console.log(`Уравнение имеет два корня. X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`);
+//         console.log(x1);
+//         return { roots: [ x1 ], D: d };
+//     } else {
+//         let x1 = (-b + Math.sqrt(d)) / (2 * a);
+//         let x2 = (-b - Math.sqrt(d)) / (2 * a);
+//         return { roots: [ x1, x2 ], D: d}
+//     };
+// };
+
+// function showSolutionsMessage(a, b, c) {
+//     let result = getSolutions(a, b, c);
+//     console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
+//     console.log(`Значение дискриминанта: ${result.D}`);
+//     if (result.D < 0) {
+//         console.log('Уравнение не имеет вещественных корней');
+//     } else if (result.D === 0) {
+//         console.log(`Уравнение имеет один корень X₁ = ${result.roots[0]}`);
+//     } else {
+//         console.log(`Уравнение имеет два корня. X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`);
+//     }
+// };
+
+// showSolutionsMessage(1, 2, 3);
+// showSolutionsMessage(7, 20, -3);
+// showSolutionsMessage(2, 4, 2);
+
+// Задание 2
+
+function getAverageScore(data) {
+    
+    let averageData = {};
+    let totalAverage = 0;
+    
+    for (key in data) {
+
+        let sum = 0;
+
+        for (let i = 0; i < data[key].length; i++) {
+            sum += data[key][i];            
+        };
+
+        let courseAverage = sum / data[key].length;
+        averageData[key] = courseAverage;
+        totalAverage += courseAverage;
     }
-};
+    
+    let courseCounter = Object.keys(data).length;
+    totalAverage /= courseCounter;
+  
+    averageData.average = totalAverage;
+    console.log(averageData);
+}
+``
 
-showSolutionsMessage(1, 2, 3);
-showSolutionsMessage(7, 20, -3);
-showSolutionsMessage(2, 4, 2);
+console.log(getAverageScore(
+    {
+        algebra: [2, 4, 5, 2, 3, 4],
+        geometru: [2, 4, 5],
+        russian: [3, 3, 4, 5],
+        physics: [5, 5],
+        music: [2, 2, 6],
+        english: [4, 4, 3],
+        poetry: [5, 3, 4],
+        chemistry: [2],
+        french: [4, 4]
+    }
+));
