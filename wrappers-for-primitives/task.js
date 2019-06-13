3162294631d6b54aec91f4eeb80d57c619f43c6c
Sergey Malinin <malininss@yandex.ru>
2019-06-13 22:26:20 +0300

    Homework 2.1 done

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+'use strict'
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -10,9 +12,43 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+	
+	//Считаем количество полных месяцев
+	
+	let dateEnd = new Date(date);
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+	let yearEnd = new Date(dateEnd).getFullYear(),
+			monthEnd = new Date(dateEnd).getMonth() + 1;
+
+
+	let yearStart = new Date().getFullYear(),
+			monthStart = new Date().getMonth() + 1;
+
+	let yearsPerMonth = (yearEnd - yearStart) *  12; 
+	let totalMonths = yearsPerMonth + (monthEnd - monthStart);
+	
+	
+	//Проверяем корректность ввода данных
+	
+	if (isNaN(percent) && typeof(percent) === 'string') {
+		console.log(`Параметр percent содержит неправильное значение ${percent}`);
+	} else { percent = parseInt(percent) };
+	
+	if (isNaN(contribution) && typeof(contribution) === 'string') {
+		console.log(`Параметр contribution содержит неправильное значение ${contribution}`);
+	} else { percent = parseInt(percent) };
+	
+	if (isNaN(amount) && typeof(amount) === 'string') {
+		console.log(`Параметр amount содержит неправильное значение ${amount}`);
+	} else { percent = parseInt(percent) };
+	
+
+	percent = percent / 100;
+	
+	let paymentPerMonth = ((amount - contribution) * ((percent / 12) + (percent / 12) / (((1 + (percent / 12)) ** totalMonths) - 1)));
+	let totalAmount = (paymentPerMonth * totalMonths).toFixed(2);
+	
+	return totalAmount;
 }
 
 function sayHello() {
@@ -24,5 +60,10 @@ function sayHello() {
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
+        console.log(name.length);
+		if (name === 'null' || name.trim() === '' || name === 'undefined') {
+            return 'Привет, Мир! Меня зовут Аноним';
+        }
+        return `Привет, Мир! Меня зовут ${name}`;
     //return greeting;
 }
