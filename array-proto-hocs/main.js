f17628dd914d1abc953a252923b4e9d61e62fe43
Sergey Malinin <malininss@yandex.ru>
2019-07-18 22:15:48 +0300

    fix errors

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -10,24 +10,42 @@ function memoize(fn, limit) {
     console.log('Переданные аргументы: ' + funcArguments);
 
     // Проверяем, были ли уже такие результаты
-    for (let item in result) {
-      console.log('Проверяем память: ' + result[item].args);
-      if (compareArrays(funcArguments, result[item].args)) {
-        return console.log('Такие аргументы уже были! Выдаём результат: ' + result[item].result);
-      }      
-    }
+    // for (let item in result) {
+    //   console.log('Проверяем память: ' + result[item].args);
+    //   if (compareArrays(funcArguments, result[item].args)) {
+    //     console.log('Такие аргументы уже были! Выдаём результат!');
+    //     return result[item].result;
+    //   }      
+    // }
 
-    console.log('Аргументов в истории не найдено, рассчитываем результат: ')
+
+    let resultCheck = '';
+
+    result.forEach(function(item) {
+
+      console.log('Проверяем память: ' + item.args);
+      if (compareArrays(funcArguments, item.args)) {
+        console.log('Такие аргументы уже были! Выдаём результат!');
+        resultCheck = item.result;
+      }   
+          
+    });
+
+    if (resultCheck) return resultCheck;
+
+    console.log('Рассчитываем результат');
+
+    // console.log('Аргументов в истории не найдено, рассчитываем результат: ')
 
     //Высчитываем результат и добавляем его в переменную
     let newResult = {
       args: funcArguments,
-      result: fn(funcArguments[0], funcArguments[1]) // Как тут получить результат работы функции, не понимаю...... Всё работает, кроме этого!
+      result: fn(funcArguments[0], funcArguments[1]) 
     };
 
     //Пушим переменную в массив result
     result.push(newResult);
-    console.log(result);
+    // console.log(result);
     
     //Смотрим, не превышен ли лимит элементов
     if (result.length > limit) {
@@ -35,8 +53,8 @@ function memoize(fn, limit) {
       result = result.slice(-limit);
       console.log(result);
     }
-
-    return console.log('Выдаём рассчитанный результат: ' + result[result.length - 1].result);
+    console.log('Выдаём рассчитанный результат!');
+    return result[result.length - 1].result;
 	}
 
 }
@@ -51,6 +69,5 @@ function sum(...values) {
 
 const mSum = memoize(sum, 3);
 
-console.log(mSum(3, 4));
 console.log(mSum(3, 4));
 console.log(mSum(3, 4));
